7c4b8dc820f0ea07852d0d2803aedd5fde8a7534
halpo (Andrew Redd) <amredd@gmail.com>
2010-06-25 23:00:59 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/pfda@64 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- tests/test_mixed.R
+++ tests/test_mixed.R
@@ -9,7 +9,7 @@ id=factor(rep(1:10,each=11)))
 pfda(y%&%z~x%|%id, data=d,k=c(1,1),df=c(2.1,2.1,2.1,2.1),driver="dual.mixed",
 		#	control=pfdaControl(C.debug=pfda:::Cdebug(200:299)))->testbase
     control=pfdaControl()) -> testbase
-
+		
 # AIC
 with(testbase, pfda:::.dual.bc.n2L(y, z, B, subject, tm, tn, tf, tg, lambda, Da, Db, sxi))
 AIC(testbase)
@@ -18,10 +18,21 @@ AIC(testbase)
 x<-seq(.5,3,length=250)
 library(ggplot2)
 #for tm
-sapply(x,function(x){ AIC(within(testbase,tm=tm*x)) })
+aic.tm<-sapply(x,function(x){ AIC( 	within.list(testbase,tm<-tm*x) 	) })
+qplot(x,aic.tm,xlab="Inflation factor",ylab='AIC',main=expression(theta[mu]),geom='line')
+#for tn
+aic.tn<-sapply(x,function(x){ AIC( 	within.list(testbase,tn<-tn*x) 	) })
+qplot(x,aic.tn,xlab="Inflation factor",ylab='AIC',main=expression(theta[nu]),geom='line')
 
+# Can optimize penalties?
+pfda(y%&%z~x%|%id, data=d,k=c(1,1),df=c(NA,2.1,2.1,2.1),driver="dual.mixed") -> testpen1
+system.time({ pfda(y%&%z~x%|%id, data=d,k=c(1,1),df=NULL,driver="dual.mixed") -> testpen })
 
-# Can optimize?
+#can optimize k?
+system.time({ pfda(y%&%z~x%|%id, data=d,k=NULL,df=c(2.1,2.1,2.1,2.1),driver="dual.mixed") -> testk })
+
+# can optimize both?
+system.time({ pfda(y%&%z~x%|%id, data=d,k=NULL,df=NULL,driver="dual.mixed") -> testboth })
 
 
 # Debugging
